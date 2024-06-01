87c1fdb2641a198b4c0eac508f268dc78e895b66
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-06-01 12:39:41 +0545

    remove recipe

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,2 @@
-export { LIST_OF_APPS, LANGUAGES, AI_PROMPTS, RECIPES } from "./constants";
+export { LIST_OF_APPS, LANGUAGES, AI_PROMPTS } from "./constants";
 export { default as uuid } from "./uuid";

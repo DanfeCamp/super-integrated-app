878d08e85394308602158d18b03eb41ca64ff95d
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-06-02 15:40:25 +0545

    remove cookie data file

--- utils/constants/index.js
+++ utils/constants/index.js
@@ -1,4 +1,3 @@
 export { default as LIST_OF_APPS } from "./apps";
 export { default as AI_PROMPTS } from "./ai-prompts";
 export { default as LANGUAGES } from "./languages";
-export { default as COOKIE_DETAILS } from "./cookie-details";
